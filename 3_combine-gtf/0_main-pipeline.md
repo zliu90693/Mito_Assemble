2ebb14a2a62db411305cc25904a5157c25146ced
zliu <zliu90693@gmail.com>
2026-04-30 01:39:57 +0800

    20260430

--- 3_combine-gtf/0_main-pipeline.md
+++ 3_combine-gtf/0_main-pipeline.md
@@ -1,9 +1,12 @@
-- Create symbolic links to the gff files output by the [2_annotate-mito](../2_annotate-mito) project and generate gtf using [1_gff2gtf.sh](./1_gff2gtf.sh).
+- Create symbolic links to the mito gff files output by the [2_annotate-mito](../2_annotate-mito) project.
   ```bash
-  cd /home/liuzhiyu/Projects/neo_caste/Mito_Assemble/3_combine-gtf
-  ./1_gff2gtf.sh
+  cd /home/liuzhiyu/Projects/neo_caste/Mito_Assemble/3_combine-gtf/annotate_out_gff
+  for path in $(find ../../2_annotate-mito -name "*.gff"); do
+      sp_name=$(basename $(dirname "$path"))
+      ln -sf $path "${sp_name}.gff"
+  done
   ```
-- Manually download the GTF annotation files and put into genome_gtf directory.
+- Manually download the GTF annotation files and put into [genome_gtf](./genome_gtf/) directory.
   ```bash
   cd /home/liuzhiyu/Projects/neo_caste/Mito_Assemble/3_combine-gtf
   wget https://ftp.ensemblgenomes.ebi.ac.uk/pub/metazoa/current/gtf/bombus_terrestris_gca910591885v2/Bombus_terrestris_gca910591885v2.iyBomTerr1.2.62.gtf.gz -O "./genome_gtf/Bombus_terrestris.gtf.gz"
@@ -11,4 +14,19 @@
   wget https://ftp.ensemblgenomes.ebi.ac.uk/pub/metazoa/current/gtf/monomorium_pharaonis_gca013373865v2/Monomorium_pharaonis_gca013373865v2.ASM1337386v2.62.gtf.gz -O "./genome_gtf/Monomorium_pharaonis.gtf.gz"
   find ./genome_gtf -name "*.gz" -exec gunzip {} \;
   ```
-- 
+- Manually obtain the GTF of mitochondrial genes from the entire *Apis mellifera* genome and save it in standard_mito_gtf as a standard reference.
+  ```bash
+  cd /home/liuzhiyu/Projects/neo_caste/Mito_Assemble/3_combine-gtf
+  wget https://ftp.ensemblgenomes.ebi.ac.uk/pub/metazoa/current/gtf/apis_mellifera/Apis_mellifera.Amel_HAv3.1.62.gtf.gz -O "./standard_mito_gtf/Apis_mellifera_genome.gtf.gz"
+  zcat "./standard_mito_gtf/Apis_mellifera_genome.gtf" | grep CM009947.2 > "./standard_mito_gtf/Apis_mellifera.gtf"
+  ```
+
+- Convert the gff output from [2_annotate-mito](../2_annotate-mito/) Project into standard-compliant gtf file according to the format of Apis_mellifera.gtf using [1_gff2formatgtf.py](./1_gff2formatgtf.py).
+
+- Merge mitochondrial GTF files with genomic GTF files lacking mitochondria.
+  ```bash
+  cd /home/liuzhiyu/Projects/neo_caste/Mito_Assemble/3_combine-gtf
+  cat "./annotate_out_gtf/Bombus_terrestris.gtf" "./genome_gtf/Bombus_terrestris.gtf" > "./combined_gtf/Bombus_terrestris.gtf"
+  cat "./annotate_out_gtf/Harpegnathos_saltator.gtf" "./genome_gtf/Harpegnathos_saltator.gtf" > "./combined_gtf/Harpegnathos_saltator.gtf"
+  cat "./annotate_out_gtf/Monomorium_pharaonis.gtf" "./genome_gtf/Monomorium_pharaonis.gtf" > "./combined_gtf/Monomorium_pharaonis.gtf"
+  ```
---
+++ 3_combine-gtf/1_gff2formatgtf.py
@@ -0,0 +1,72 @@
+# %%
+
+import pyranges as pr
+import pandas as pd
+
+# %%
+
+def gff2gtf(sp_name):
+    gff = pr.read_gff3(f"./annotate_out_gff/{sp_name}.gff")
+    df = gff.as_df()
+    df.drop(["Is_circular", "genome", "mol_type", "Frame", "ID", "Score", "gene_id", "Parent"], axis = 1, inplace=True)
+    df.drop(df.index[0], inplace=True) # The first line contains the complete mitochondrial information and needs to be deleted.
+    df = df[~(df["Name"].str.startswith('trn', na=False) | df["Name"].str.startswith('rrn', na=False))] # remove tRNA & rRNA
+
+    df = df.rename(columns={'Name': 'gene_id'}) # add gene_id gene_name etc.
+    df["transcript_id"] = df["gene_id"] + "_t1"
+    df["gene_name"] = "MT-" + df["gene_id"]
+    df["gene_biotype"] = "protein_coding"
+
+    gene_rows = df[df['Feature'] == 'gene'].copy()
+    gene_rows['Feature'] = 'transcript'
+    df = pd.concat([df, gene_rows], ignore_index=True).sort_values(by=["Start"]).reset_index(drop=True)  
+    # Order: gene, transcript, exon
+    feature_order = {'gene': 0, 'transcript': 1, 'exon': 2}
+    # sort gene_id by sort_key
+    df['sort_key'] = df['Feature'].map(feature_order)
+    df = df.sort_values(['gene_id', 'sort_key']).drop(columns='sort_key').reset_index(drop=True)
+
+    new_pr = pr.PyRanges(df)
+    new_pr.to_gtf(f"./annotate_out_gtf/{sp_name}.gtf")
+
+# %%
+
+gff2gtf("Bombus_terrestris")
+gff2gtf("Harpegnathos_saltator")
+gff2gtf("Monomorium_pharaonis")
+
+
+# %%
+
+# Bter_gff = pr.read_gff3("./annotate_out_gff/Bombus_terrestris.gff")
+# Bter_gff
+
+# # %%
+
+# Bter_df = Bter_gff.as_df()
+# Bter_df.drop(["Is_circular", "genome", "mol_type", "Frame", "ID", "Score", "gene_id", "Parent"], axis = 1, inplace=True)
+# Bter_df.drop(Bter_df.index[0], inplace=True) # The first line contains the complete mitochondrial information and needs to be deleted.
+# Bter_df = Bter_df[~(Bter_df["Name"].str.startswith('trn', na=False) | Bter_df["Name"].str.startswith('rrn', na=False))] # remove tRNA & rRNA
+
+# Bter_df = Bter_df.rename(columns={'Name': 'gene_id'})
+# Bter_df["transcript_id"] = Bter_df["gene_id"] + "_t1"
+# Bter_df["gene_name"] = "MT-" + Bter_df["gene_id"]
+# Bter_df["gene_biotype"] = "protein_coding"
+
+# # add transcript in Features column
+# Bter_gene_rows = Bter_df[Bter_df['Feature'] == 'gene'].copy()
+# Bter_gene_rows['Feature'] = 'transcript'
+# Bter_df = pd.concat([Bter_df, Bter_gene_rows], ignore_index=True).sort_values(by=["Start"]).reset_index(drop=True)
+# # Order: gene, transcript, exon
+# feature_order = {'gene': 0, 'transcript': 1, 'exon': 2}
+# # sort gene_id by sort_key
+# Bter_df['sort_key'] = Bter_df['Feature'].map(feature_order)
+# Bter_df = Bter_df.sort_values(['gene_id', 'sort_key']).drop(columns='sort_key').reset_index(drop=True)
+# Bter_df
+
+# # %%
+
+# Bter_pr = pr.PyRanges(Bter_df)
+# Bter_pr.to_gtf("./annotate_out_gtf/Bombus_terrestris_pr.gtf")
+
+# # %%
