4b6c8faa8cd69375dc466e575ee9e9be3f5faa28
zliu <zliu90693@gmail.com>
2026-05-02 17:58:06 +0800

    After download fastq

--- 1_generate-mito/0_main-pipeline.md
+++ 1_generate-mito/0_main-pipeline.md
@@ -4,7 +4,9 @@
   - Lasioglossum albipes
 
 # Lasioglossum zephyrus
+- By checking Genbank, the SRR corresponding to LZEP_v2.2.0 was determined to be SRR11352862.
 - Download the FASTQ files for the project corresponding to the *Lasioglossum zephyrus* reference genome using [1_get-fastq.sh](./1_get-fastq.sh)
   ```bash
+  cd /home/liuzhiyu/Projects/neo_caste/Mito_Assemble/1_generate-mito
   ./1_get-fastq.sh -s SRR11352862 -n Lasioglossum_zephyrus
   ```
--- 1_generate-mito/1_get-fastq.sh
+++ 1_generate-mito/1_get-fastq.sh
@@ -18,6 +18,17 @@ while getopts "s:n:" opt; do
     esac
 done
 
+sra_path=$(srapath $srr_id)
+
+# mkdir -p "./sra/${sp_name}"
+aria2c -x 16 -s 16 -o "./sra/${sp_name}.lite.1" $sra_path
+
 mkdir -p "./fastq/${sp_name}"
-cd "./fastq/${sp_name}"
-kingfisher get -r "${srr_id}" -m ena-ftp aws-http prefetch
+# kingfisher get -r "${srr_id}" -m ena-ftp aws-http prefetch
+fasterq-dump "./sra/${sp_name}.lite.1" \
+    --split-files \
+    --threads 8 \
+    --outdir "./fastq/${sp_name}" \
+    --temp "./fastq/${sp_name}/.tmp" \
+    --progress
+pigz -p 16 "./fastq/${sp_name}/"*.fastq
